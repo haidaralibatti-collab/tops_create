cfd47d61baf2d69783f1d8b7ceccd9b5ade89c2a
Haidarali Batti <haidaralibatti039@gmail.com>
2026-02-23 10:04:23 +0530

    Delete python calss/py_02.py

--- python calss/py_02.py
+++
@@ -1,5 +0,0 @@
-main_string = ("Enter the main string:")
-sub_string = ("Enter the substring to search:")
-
-count = main_string.count(sub_string)
-print("The substring appears", count, "times in the given string.")
